dbde28b7f69b1151670864a14d56a9e197a32098
Arek Ogórek <ogurkowo@gmail.com>
2020-07-21 21:49:15 +0200

    Updates

---
+++ precache-manifest.93e4b6009333555cc940b595d4101be3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e243f95c62a1f70eae1b52b56a607fae",
+    "url": "/into-good-hands/index.html"
+  },
+  {
+    "revision": "136c8920c87fc5e01cf9",
+    "url": "/into-good-hands/static/css/main.400101ee.chunk.css"
+  },
+  {
+    "revision": "2d04999d38296166c0ed",
+    "url": "/into-good-hands/static/js/2.b5ac685d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/into-good-hands/static/js/2.b5ac685d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136c8920c87fc5e01cf9",
+    "url": "/into-good-hands/static/js/main.46938cc7.chunk.js"
+  },
+  {
+    "revision": "fe51156df1c4e6e650e2",
+    "url": "/into-good-hands/static/js/runtime-main.af13ac0a.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/into-good-hands/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/into-good-hands/static/media/Columns-Background.99f2e620.png"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/into-good-hands/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/into-good-hands/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/into-good-hands/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/into-good-hands/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/into-good-hands/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/into-good-hands/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/into-good-hands/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/into-good-hands/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/into-good-hands/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/into-good-hands/static/media/Signature.e54cfcfc.svg"
+  }
+]);
